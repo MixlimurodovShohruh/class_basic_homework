89dde57342b6b236d18dbf5fa57d6a8a92dc0ae3
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-02-13 12:07:23 +0500

    finished

--- create_one_object.py
+++ create_one_object.py
@@ -1,3 +1,4 @@
 from create_one_attribute import Person
+person=Person("Ali")
 
 #create an object named "person" whose name is "Ali"
--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -1,5 +1,16 @@
 from create_one_attribute import Person
+p1=Person("Anvar")
+p2=Person("Shavkat")
+p3=Person("Jasur")
+a=p1.name
+b=p2.name
+c=p3.name
+ls=[]
+ls.append(a)
+ls.append(b)
+ls.append(c)
 
+print(ls)
 #Create an object named "p1" whose name is "Anvar"
 #Create an object named "p2" whose name is "Shavkat"
 #Create an object named "p3" whose name is "Jasur"
